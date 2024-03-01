deb7810f0c72fee3c0a92d440c833c46fe40fb7d
31692315 <161085035+31692315@users.noreply.github.com>
2024-03-01 17:02:30 -0500

    Add files via upload

---
+++ Ejercicios_operadores/operadores/cuadrado.js
@@ -0,0 +1,17 @@
+console.log("5. Calcular el área y el perimetro de un cuadrado: ");
+console.log("Ingrese cateto del cuadrado: ");
+let cateto = 10;
+console.log(cateto)
+
+//proceso area
+let resultado_a = (cateto * cateto);
+//proceso perimetro
+let resultado_p = (cateto * 4);
+
+//salida área
+console.log("El área del cuadrado es: ")
+console.log(resultado_a);
+
+//salida área
+console.log("El perimetro del cuadrado es: ")
+console.log(resultado_p);
---
+++ Ejercicios_operadores/operadores/euroDolar.js
@@ -0,0 +1,13 @@
+console.log("4. Convertir de euros a dolares: ");
+console.log("Ingrese la cantidad de euros que va a cambiar a dolares: ");
+let cant_euros = 500;
+console.log(cant_euros)
+console.log("Ingrese el valor del euro en dolares: ");
+let dolar = 1.08;
+console.log(dolar)
+
+//proceso
+let resultado = (cant_euros * dolar);
+
+//salida
+console.log(resultado);
