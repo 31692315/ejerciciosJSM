c89acae458e310a68509884eab04777b7307c0eb
31692315 <161085035+31692315@users.noreply.github.com>
2024-03-08 16:02:15 -0500

    Add files via upload

---
+++ ejerciciosJMS/operadores/sumas.js
@@ -0,0 +1,13 @@
+console.log("2. Sumar dos números: ");
+console.log("Ingrese primer número: ");
+let num1 = 15;
+console.log(num1)
+console.log("Ingrese segundo número: ");
+let num2 = 20;
+console.log(num2)
+
+//proceso
+let resultado = (num1 + num2);
+
+//salida
+console.log(resultado);
